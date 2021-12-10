90be748a553a8e75f704863eabc195bad717d8a3
Emilie Gouy <vibesession@gmail.com>
2021-12-10 12:26:57 +0100

    app back fonctionelle

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,41 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+      if @movie.save
+       redirect_to movies_path
+      else
+        render :new
+      end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  #private
+
+  def set_movies
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :rating)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :movies do
+    resources :bookmarks, only: [ :new, :create ]
+    resources :lists, except: [:edit, :update]
+  end
+
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+
+  resources :bookmarks, only: [ :destroy ]
+  resources :lists, only: [ :index, :show, :edit, :new, :update, :create, :delete ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
